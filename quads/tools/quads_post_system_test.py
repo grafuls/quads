c57b8689b53218d48f40917b933eecbb6c453faf
Gonzalo Rafuls <grafuls@gmail.com>
2019-02-11 20:58:15 +0100

    Fixes to quads post system tests
    Change-Id: Icd7e174fa9e9ebcc22c26a0ccbe194f09fc42ebb

--- quads/tools/quads_post_system_test.py
+++ quads/tools/quads_post_system_test.py
@@ -3,32 +3,33 @@
 import argparse
 import os
 import requests
-import sys
 import urllib3
 
 from quads.config import conf as quads_config
+from quads.quads import Api as QuadsApi
+from quads.tools.foreman import Foreman
+
+API = 'v2'
 
 
 def main(argv=None):
-    quads_config_file = os.path.dirname(__file__) + "/../conf/quads.yml"
+
+    api_url = os.path.join(quads_config['quads_base_url'], 'api', API)
+    quads = QuadsApi(api_url)
 
     urllib3.disable_warnings()
 
     if "data_dir" not in quads_config:
-        print("quads: Missing \"data_dir\" in " + quads_config_file)
+        print("quads: Missing \"data_dir\" in configuration.")
         exit(1)
 
     if "install_dir" not in quads_config:
-        print("quads: Missing \"install_dir\" in " + quads_config_file)
+        print("quads: Missing \"install_dir\" in configuration.")
         exit(1)
 
     if "quads_base_url" not in quads_config:
-        print("quads: Missing \"quads_base_url\" in " + quads_config_file)
+        print("quads: Missing \"quads_base_url\" in configuration.")
         exit(1)
-
-    sys.path.append(quads_config["install_dir"] + "/lib")
-    sys.path.append(os.path.dirname(__file__) + "/../lib")
-    from requests.auth import HTTPBasicAuth
 
     parser = argparse.ArgumentParser(description='Query current hosts marked for build')
     parser.add_argument('--cloud', dest='cloud', type=str, default=None, help='specify the cloud to query')
@@ -39,33 +40,30 @@ def main(argv=None):
     # need to determine the ticket / password
     if args.cloud:
         # post
-        url = quads_config["quads_base_url"] + "api/v1/lstickets"
-        headers = {'Content-Type': 'application/json'}
-        data = {"cloudonly": args.cloud}
-        r = requests.post(url, data, headers=headers)
-        js = r.json()
-        ticket_value = None
-        if 'ticket' in js:
-            for owner in js['ticket']:
-                ticket_value = owner[args.cloud]
+        cloud_details = quads.get_clouds(name=args.cloud)
+        if 'ticket' in cloud_details:
+            ticket_value = cloud_details["ticket"]
 
         if not ticket_value:
             ticket_value = quads_config['ipmi_password']
-        url = quads_config['foreman_api_url'] + "/hosts?search=build=true"
-        headers = {'Content-Type': 'application/json'}
-        r = requests.get(url, headers=headers,
-                         auth=HTTPBasicAuth(args.cloud, ticket_value),
-                         verify=False)
-        js = r.json()
-        if 'results' not in js:
+
+        foreman = Foreman(
+            quads_config["foreman_api_url"],
+            args.cloud,
+            ticket_value,
+        )
+
+        hosts_to_build = foreman.get_parametrized("build", "true")
+
+        if 'results' not in hosts_to_build:
             print("Unable to query foreman for cloud: " + args.cloud)
             print("Verify foreman password is correct: " + ticket_value)
             exitcode = 1
         else:
-            if len(js['results']) > 0:
+            if len(hosts_to_build['results']) > 0:
                 print("The following hosts are marked for build:")
                 print("")
-            for h in js['results']:
+            for h in hosts_to_build['results']:
                 print(h['name'])
                 exitcode = 1
 
